5882c3a75e136bac610cc2f4be843c102b9a4130
Vishal Arak <79998982+vishalarak@users.noreply.github.com>
2021-09-20 06:55:28 -0700

    Add files via upload

---
+++ 3divisible.cpp
@@ -0,0 +1,16 @@
+//using break and continue statement
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    for (int i=0; i<100; i++)
+    {
+        if(i%3==0)
+        {
+            continue;
+        }
+        cout<<i<<endl;
+    }
+    return 0;
+}
